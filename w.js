304eea14bd83dc0901a57ac3623210832b66a590
nanudoldirmgjj <86347090+nanudoldirmgjj@users.noreply.github.com>
2022-12-13 19:08:08 +0300

    Add files via upload

---
+++ w.js
@@ -0,0 +1,41 @@
+/*
+Задание 3
+
+Самостоятельно реализовать меню, открывающееся по клику на бургер, который тут же превращается в крестик.
+
+Образец - devby.io, или можно посмотреть здесь: https://ucarecdn.com/aeb69a87-...
+
+Внутри открывающегося по клику на бургер блока сначала идет строка поиска, при клике в нее (фокусное состояние) фон всей строки, включая кнопку, меняется на белый.
+
+Все нужные иконки скачать с сайта dev.by, из кода в инспекторе.
+
+ПОДСКАЗКИ. Вы можете изначально сверстать открытое меню, при этом кнопка "крестик" будет расположена в точности на месте кнопки "бургер". Скрыть выпадающий блок с помощью CSS. При открытии меню кнопка "бургер" будет исчезать, при закрытии - появляться.
+*/
+const on = document.getElementById('on');
+const off = document.getElementById('off');
+const search = document.getElementById('search');
+const input = document.getElementById('i');
+const button =document.getElementById('b');
+
+on.onclick = () => {
+    on.style.opacity = '0';
+    on.style.visibility = 'hidden';
+    off.style.opacity = '1';
+    off.style.visibility = 'visible';
+    search.style.opacity = '1';
+    search.style.visibility = 'visible';
+}
+input.onclick = () => {
+    input.style.background = 'white';
+    button.style.background = 'white';
+}
+off.onclick = () => {
+    on.style.opacity = '1';
+    on.style.visibility = 'visible';
+    off.style.opacity = '0';
+    off.style.visibility = 'hidden';
+    search.style.opacity = '0';
+    search.style.visibility = 'hidden';
+    input.style.background = '#F5F5F7';
+    button.style.background = '#F5F5F7';
+}
